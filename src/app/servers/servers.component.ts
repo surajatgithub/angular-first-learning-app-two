ad6f590001d15b14fa79b96cba1e2a56f9bacb87
Suraj Yadav <syadav@verif-y.com>
2022-02-10 13:39:37 +0530

    master - directives

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  serverName: string = '';
+  servers: string[] = [];
+  displayDetailsCheck = false;
+  displayDetailsToggleLog: string[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onHoldNewServer(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
+  onCreateNewServer() {
+    this.servers.push(this.serverName);
+    this.serverName = '';
+    console.log(this.servers);
+  }
+
+  onToggleDisplayDetailsCheck() {
+    this.displayDetailsCheck = !this.displayDetailsCheck;
+    this.displayDetailsToggleLog.push(new Date().toUTCString());
+  }
+
+  getLogBackgroundColor(index: number) {
+    if (index >=5) {
+      return 'blue';
+    }
+    return 'yellow';
+  }
 }
